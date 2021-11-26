1ad561fa28a3b2d2d72cceca0ae74c2abbb3df33
Garbuzov Zakhar <zakhar-garbuzov@inbox.ru>
2021-11-26 22:34:57 +0300

    creation 1000

---
+++ 1000.cpp
@@ -0,0 +1,20 @@
+// https://acm.timus.ru/problem.aspx?space=1&num=1000
+// timus 1000
+
+
+#include <iostream>
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+
+    std::cin >> a;
+    std::cin >> b;
+
+    int sum = a + b;
+
+    std::cout << sum;    
+
+    return 0;
+}
