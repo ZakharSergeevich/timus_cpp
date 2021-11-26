5866b7b5020a41f0287449fc8d9552a1c030baf4
Garbuzov Zakhar <zakhar-garbuzov@inbox.ru>
2021-11-27 00:02:34 +0300

    creation 1068

---
+++ 1068.cpp
@@ -0,0 +1,36 @@
+// https://acm.timus.ru/problem.aspx?space=1&num=1068
+// 1068
+
+
+#include <iostream>
+
+int main()
+{
+    int num = 0;
+    std::cin >> num;
+
+    int sum = 0;
+    
+    if (num == 1)
+    {
+        sum = 1;
+    } 
+    else if (num > 1)
+    {
+        for (int i = 1; i <= num; i++)
+        {
+            sum = sum + i;
+        }  
+    }
+    else if (num < 1)
+    {
+        for (int i = 1; i >= num; i--)
+        {
+            sum = sum + i;
+        }
+    }
+
+    std::cout << sum;
+    
+    return 0;
+}
