513406668bf45dbd5efc7ce303b3bfe1d70f2b13
Garbuzov Zakhar <zakhar-garbuzov@inbox.ru>
2021-12-22 22:55:06 +0300

    creation 1925

---
+++ 1925.cpp
@@ -0,0 +1,34 @@
+// https://acm.timus.ru/problem.aspx?space=1&num=1925
+// timus 1925
+
+
+#include <iostream>
+#include <vector>
+
+
+struct Nums
+{
+    int displayedNum;
+    int enteredNum;
+};
+
+
+std::string getBangMessage();
+
+
+int main()
+{
+    int n = 0;
+    int k = 0;
+
+    std::vector <Nums> numsHistory;
+
+
+    return 0;
+}
+
+
+std::string getBangMessage()
+{
+    return "Big Bang!";
+}
