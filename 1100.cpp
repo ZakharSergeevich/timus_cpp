34a092aa849ce47f42d2160d80be3a29d0b067d6
Garbuzov Zakhar <zakhar-garbuzov@inbox.ru>
2022-01-25 23:57:49 +0300

    creation 1100

---
+++ 1100.cpp
@@ -0,0 +1,66 @@
+// https://acm.timus.ru/problem.aspx?space=1&num=1100
+// timus 1100
+
+
+#include <iostream>
+#include <vector>
+
+
+struct Pair
+{
+    int id = 0;
+    int m = 0;
+};
+
+
+
+int main()
+{
+    int dataNums = 0;
+    std::cin >> dataNums;
+
+    std::vector <Pair> commandsNum;
+
+    for (int i = 0; i < dataNums; i++)
+    {
+        Pair command;
+        
+        std::cin >> command.id;
+        std::cin >> command.m;
+        
+        commandsNum.push_back(command);
+    }
+
+    std::vector <Pair> commandsNumSorted;
+    commandsNumSorted = commandsNum;
+
+    // -- sort
+
+    Pair buff;
+
+    for (int i = 0; i < dataNums; i++)
+        for (int j = dataNums - 1; j > i; j--)
+        {
+            if (commandsNumSorted[j - 1].m > commandsNumSorted[j].m)
+            {
+                buff = commandsNumSorted[j - 1];
+                commandsNumSorted[j - 1] = commandsNumSorted[j];
+                commandsNumSorted[j] = buff;
+            }
+            //if (commandsNumSorted[j - 1].m == commandsNumSorted[j].m)
+            //{
+            //    if (commandsNumSorted[j - 1].id > commandsNumSorted[j].id)
+            //    {
+            //        buff = commandsNumSorted[j - 1];
+            //        commandsNumSorted[j - 1] = commandsNumSorted[j];
+            //        commandsNumSorted[j] = buff;
+            //    }
+            //}
+        }
+
+
+    for (int i = dataNums - 1; i >= 0; i--)
+        std::cout << commandsNumSorted[i].id << ' ' << commandsNumSorted[i].m << std::endl;
+
+    return 0;
+}
